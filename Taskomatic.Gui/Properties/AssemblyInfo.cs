9af876b8d9d4b9d89e30e98171d5d4cf90a8f772
Friedrich von Never <friedrich@fornever.me>
2017-02-24 19:21:10 +0700

    AssemblyInfo: remove unused fields

--- Taskomatic.Gui/Properties/AssemblyInfo.cs
+++ Taskomatic.Gui/Properties/AssemblyInfo.cs
@@ -4,8 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Taskomatic.Gui")]
 [assembly: AssemblyProduct("Taskomatic")]
 [assembly: AssemblyCopyright("Copyright © F. von Never 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("869b83c8-984e-469f-aacf-7eaf5db97d43")]
 [assembly: AssemblyVersion("0.0.1.0")]
