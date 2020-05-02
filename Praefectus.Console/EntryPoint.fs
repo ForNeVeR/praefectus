8ea94ed4f411016064ba4352b23903ea6caf4a28
Friedrich von Never <friedrich@fornever.me>
2020-05-02 22:28:35 +0700

    Add logging (#5)

--- Praefectus.Console/EntryPoint.fs
+++ Praefectus.Console/EntryPoint.fs
@@ -1,6 +1,28 @@
 ﻿module Praefectus.EntryPoint
 
+open System.Reflection
+
+open Serilog
+
+let private version = Assembly.GetEntryAssembly().GetName().Version
+
+let private createLogger() =
+    LoggerConfiguration().WriteTo.Console().CreateLogger()
+
+module ExitCodes =
+    let Success = 0
+    let Error = 1
+
 [<EntryPoint>]
-let main argv =
-    printfn "This is Praefectus."
-    0
+let main _ =
+    use logger = createLogger()
+
+    logger.Information("Praefectus v{Version}", version)
+    try
+        logger.Information "You have so many things to do today!"
+        logger.Information "Bye!"
+        ExitCodes.Success
+    with
+    | ex ->
+        logger.Error(ex, "Error when running application")
+        ExitCodes.Error
