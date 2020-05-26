0c6ced4cdf15ba9ac22e88918c34e5ffb6241c06
Friedrich von Never <friedrich@fornever.me>
2020-05-26 21:32:59 +0700

    Add comments for the Get-Version script

--- scripts/Get-Version.ps1
+++ scripts/Get-Version.ps1
@@ -1,3 +1,14 @@
+<#
+    .SYNOPSIS
+        The purpose of this script is to extract the version information from the published artifact, and to print it
+        to the standard output.
+
+        It is used during CI builds to generate name for the artifact to upload.
+    .PARAMETER SourceRoot
+        Path to the repository source root.
+    .PARAMETER MainExecutable
+        Full path to the executable to extract the version information from.
+#>
 param (
     [string] $SourceRoot = "$PSScriptRoot/..",
     [string] $MainExecutable = "$SourceRoot/publish/praefectus.dll"
