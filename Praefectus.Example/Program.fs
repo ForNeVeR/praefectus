c96322766cb7bdbfe0f3f588a7d25298a2ab4add
Friedrich von Never <friedrich@fornever.me>
2020-12-20 17:42:20 +0700

    #43: update example application code

--- Praefectus.Example/Program.fs
+++ Praefectus.Example/Program.fs
@@ -1,8 +1,11 @@
 ﻿open System
 
+open System.IO
+
 open Praefectus.Console
 
 [<EntryPoint>]
-let main argv =
+let main (argv: string[]): int =
+    let config = { DatabaseLocation = Path.Combine(Environment.CurrentDirectory, "data.json") }
     use env = Environment.OpenStandard()
-    EntryPoint.run { DatabaseLocation = Environment.CurrentDirectory } argv env
+    EntryPoint.run config argv env
