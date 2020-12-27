2655c1467b7487b281455d9c377ac3b7d9a96ea5
Friedrich von Never <friedrich@fornever.me>
2020-12-27 14:12:55 +0700

    #43: list command: better output

--- Praefectus.Console/Commands.fs
+++ Praefectus.Console/Commands.fs
@@ -1,17 +1,27 @@
 ﻿module Praefectus.Console.Commands
 
 open System.IO
-open System.Threading.Tasks
 
 open FSharp.Control.Tasks
 
+open Praefectus.Core
 open Praefectus.Storage
 
-let doList (app: Application) (isJson: bool) (output: Stream): Task<unit> = task {
+let private orderTasks =
+    Seq.sortBy (fun t -> t.Order, t.Id, t.Name)
+
+let private printTask task =
+    let print key = function
+        | Some value -> string value
+        | None -> sprintf "[NO %s]" key
+
+    printfn "%s : %s : %s" (print "ORDER" task.Order) (print "ID" task.Id) (print "NAME" task.Name)
+
+let doList (app: Application) (isJson: bool) (output: Stream): System.Threading.Tasks.Task<unit> = task {
     let! database = MarkdownDirectory.readDatabase app.Config.DatabaseLocation
     let tasks = database.Tasks
     if isJson then
         do! Json.serializeData tasks output
     else
-        tasks |> Seq.iter (printfn "%A")
+        tasks |> orderTasks |> Seq.iter printTask
 }
