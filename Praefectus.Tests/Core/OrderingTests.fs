cd787d8630d6771b1ab5c8e37cf1aefab01d6112
Friedrich von Never <friedrich@fornever.me>
2021-01-06 18:52:42 +0700

    #43: plan additional test cases for file-constrained ordering

--- Praefectus.Tests/Core/OrderingTests.fs
+++ Praefectus.Tests/Core/OrderingTests.fs
@@ -1,12 +1,15 @@
 module Praefectus.Tests.Core.OrderingTests
 
+open System
+
 open Xunit
 
 open Praefectus.Core
 open Praefectus.Storage
 
-let private generateTasksByName = Seq.map (fun name ->
-    { Task.Empty<_> { FileSystemStorage.FileName = sprintf "%s.md" name } with
+let private generateTasksByName = Seq.mapi (fun i name ->
+    { Task.Empty<_> { FileSystemStorage.FileName = sprintf "%d.%s.md" i name } with
+        Order = Some i
         Name = Some name }
 )
 
@@ -46,6 +49,58 @@ let ``reorder should move unordered items to the end``(): unit =
     let reordered = Ordering.reorder predicates tasks
     let lastTask = Seq.last reordered
     Assert.Equal(Some "ca", lastTask.Name)
+
+[<Fact>]
+let ``applyOrderInStorage should do nothing if order is already right``(): unit =
+    let tasks = generateTasksByName [| "a"; "b"; "c"; "d"; "e"; "f" |]
+    let instructions = Ordering.applyOrderInStorage tasks
+    Assert.Equal(Array.empty, instructions)
+
+[<Fact>]
+let ``applyOrderInStorage order test case 1``(): unit =
+    let tasks = generateTasksByName [| "b"; "c"; "d"; "a" |] |> Seq.cache
+    let a = Seq.last tasks
+    let reorderedTasks = seq {
+        a
+        yield! Seq.take 4 tasks
+    }
+
+    let instructions = Ordering.applyOrderInStorage reorderedTasks |> Seq.toArray
+    Assert.Equal(4, instructions.Length)
+
+    let isTask fileName =
+        let attributes = FileSystemStorage.readFsAttributes fileName
+        Action<_>(fun (instruction: StorageInstruction<FileSystemStorage.FileSystemTaskState>) ->
+            Assert.Equal(attributes.Name, instruction.Task.Name)
+            Assert.Equal(fileName, instruction.NewState.FileName)
+        )
+
+    Assert.Collection(instructions,
+                      isTask "1.a.md",
+                      isTask "2.b.md",
+                      isTask "3.c.md",
+                      isTask "4.d.md")
+
+[<Fact>]
+let ``applyOrderInStorage order test case 2``(): unit =
+    // TODO: Test case for:
+    //   1.a.md ; 2.c.md ; 3.b.md ; 4.d.md
+    // → 1.a.md ; 2.b.md ; 3.c.md ; 4.d.md
+    Assert.True false
+
+[<Fact>]
+let ``applyOrderInStorage order test case 3``(): unit =
+    // TODO: Test case for:
+    //   1.a.md ; 2.b.md ; 3.b.md ; 4.d.md (switch order for two files named *.b.md)
+    // → 1.a.md ; 3.b.md ; 4.b.md ; 5.d.md
+    Assert.True false
+
+[<Fact>]
+let ``applyOrderInStorage order test case 4``(): unit =
+    // TODO: Test case for:
+    //   1.a.md ; 3.c.md ; 4.b.md ; 5.d.md
+    // → 1.a.md ; 2.b.md ; 3.c.md ; 5.d.md
+    Assert.True false
 
 [<Fact>]
 let ``applyOrderInStorage should rename minimal amount of items``(): unit =
