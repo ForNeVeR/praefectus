01b01ef78e03fa2d2fcc0a5c70263c7297740467
Friedrich von Never <friedrich@fornever.me>
2021-02-07 19:27:01 +0700

    #43: update the example

--- Praefectus.Example/Program.fs
+++ Praefectus.Example/Program.fs
@@ -6,6 +6,9 @@ open Praefectus.Console
 
 [<EntryPoint>]
 let main (argv: string[]): int =
-    let config = { DatabaseLocation = Environment.CurrentDirectory }
+    let config = {
+        DatabaseLocation = Environment.CurrentDirectory
+        Ordering = [|  |]
+    }
     use env = Environment.OpenStandard()
     EntryPoint.run config argv env
