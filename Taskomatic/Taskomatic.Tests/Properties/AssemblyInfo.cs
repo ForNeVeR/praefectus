ff920bc4e3f36fd0352f90ae59cf875afcdae563
Friedrich von Never <friedrich@fornever.me>
2022-12-04 18:56:27 +0100

    (#57) Taskomatic: convert tests to dotnet test runner-compatible

--- Taskomatic/Taskomatic.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Taskomatic.Tests")]
-[assembly: AssemblyProduct("Taskomatic")]
-[assembly: AssemblyCopyright("Copyright © F. von Never 2017")]
-[assembly: ComVisible(false)]
-[assembly: Guid("c3fcfe44-ed76-456c-8e14-e3ec816cfeb8")]
-[assembly: AssemblyVersion("0.0.1.0")]
